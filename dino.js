8e84136120a6ffe92a35a28a70e15539cc8fbff3
Hastur456 <100359830+Hastur456@users.noreply.github.com>
2022-11-16 11:46:19 +0500

    Add files via upload

---
+++ dino.js
@@ -0,0 +1,24 @@
+const dino = document.getElementById('TRex');
+const cactus = document.getElementById('cactus');
+
+document.addEventListener('keydown', function(event) {
+    jump();
+});
+
+function jump() {
+    if (dino.classList != 'jump') {
+        dino.classList.add('jump')
+    }
+    setTimeout(function() {
+        dino.classList.remove('jump')}, 600
+    );
+}   
+
+setInterval(function () {
+    const dinosTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
+    const cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
+
+    if (cactusLeft < 50 && cactusLeft > 0 && dinosTop >= 100) {
+        alert('Game Over')
+    }
+}, 10);
